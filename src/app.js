5630f7c7d55e8f464d0dcc0790e51be9abbd8179
SAMBHAVJI <sambhavtrivedi84@gmail.com>
2025-05-29 21:14:03 +0530

    App.get and post

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use("/home", (req, res, next) => {
+app.get("/home", (req, res, next) => {
     res.send("Hi this is dashboard....");
 });
 
-app.use((req, res) => {
-    res.send("Hi this is Sambhav Trivedi....");
-});
+app.post("/user",(req,res)=>{
+    res.send("Data Succesfully offloaded to DB")
+})
 
 app.listen(3000, () => {
     console.log("The server was connected successfully at port 3000");
-});
+})
